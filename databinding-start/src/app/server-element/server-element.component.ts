1fd0924456536585f93da20dcceaeac966792c22
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-12-06 23:00:25 -0300

    Aula 72

---
+++ databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+
+  @Input('server') element: { type: string, name: string, content: string };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
