098b794f0ab6c77d91ecd766f60ed89caf4c1aac
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-01-06 22:35:21 -0300

    Finalizado - Reactive Forms (Assignment)

--- lessons/forms/forms-reactive-start/src/app/app.module.ts
+++ lessons/forms/forms-reactive-start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
---
+++ lessons/forms/forms-reactive-start/src/app/assignment/assignment.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-assignment',
+  templateUrl: './assignment.component.html',
+  styleUrls: ['./assignment.component.css']
+})
+export class AssignmentComponent implements OnInit {
+
+  form: FormGroup;
+  statusList: string[] = ['Stable', 'Critical', 'Finished'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      project: new FormGroup({
+        name: new FormControl(null, [Validators.required, this.forbiddenProjectName]),
+        status: new FormControl('Stable', Validators.required),
+      }),
+      email: new FormControl(null, [Validators.required, Validators.email])
+    });
+  }
+
+  forbiddenProjectName(control: FormControl): { [key: string]: boolean } {
+    if (control.value == 'test' || control.value == 'Test') {
+      return { 'nameIsForbidden': true }
+    }
+
+    return null;
+  }
+
+  asyncForbiddenProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value == 'test' || control.value == 'Test') {
+          resolve({ 'nameIsForbidden': true });
+        }
+    
+        resolve(null);
+      }, 1500);
+    });
+
+    return promise;
+  }
+
+  hasError(control: string, validator?: string): boolean | AbstractControl{
+    if (validator) {
+      return this.form.get(control)[validator];
+    }
+
+    return this.form.get(control);
+  }
+
+  onSubmit(): void {
+    let { email, project: { name, status } } = this.form.value;
+    alert(`${name} with status ${status.toLowerCase()} submitted by ${email}.`);
+  }
+
+  onReset(): void {
+    this.form.reset();
+  }
+
+}
